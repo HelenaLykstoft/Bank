c769f8a8517c7214f18eab86a8abc83b6574ba77
Helena Lykstoft <helena.botn.lykstoft@gmail.com>
2022-09-02 10:03:17 +0200

    creates menu

--- src/Bank.java
+++ src/Bank.java
@@ -11,5 +11,4 @@ public class Bank {
         }
         return false;
     }
-
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
+
 public class Menu {
+
+    TextUI textUI = new TextUI();
+    Bank bank = new Bank();
+
+    public void mainMenu(){
+        boolean isOn = true;
+        String[] choices = {"Add account","Delete account","Withdraw","Deposit","See transactions","quit"};
+        int choice = textUI.select("This is the menu.",choices,":)");
+
+
+        while (isOn){
+            switch (choice){
+                case 0:
+                    bank.createAccount(new Account(textUI.get(), textUI.getInteger()));
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+
+            }
+
+        }
+    }
 }
